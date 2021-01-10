f83d11d8b405fb636c03366eb9d762d73cde7a8b
Sapan <sapan.jain@lntinfotech.com>
2021-01-10 14:32:22 +0530

    all required components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { AddQuestionComponent } from './add-question/add-question.component';
+import { RemoveQuestionComponent } from './remove-question/remove-question.component';
+import { GenerateReportComponent } from './generate-report/generate-report.component';
+import { ViewReportComponent } from './view-report/view-report.component';
+import { SearchStudentComponent } from './search-student/search-student.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { SelectExamComponent } from './select-exam/select-exam.component';
+import { StartExamComponent } from './start-exam/start-exam.component';
+import { QuestionPaperComponent } from './question-paper/question-paper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    RegistrationComponent,
+    UserLoginComponent,
+    AdminLoginComponent,
+    ResetPasswordComponent,
+    AddQuestionComponent,
+    RemoveQuestionComponent,
+    GenerateReportComponent,
+    ViewReportComponent,
+    SearchStudentComponent,
+    HomeComponent,
+    AboutUsComponent,
+    SelectExamComponent,
+    StartExamComponent,
+    QuestionPaperComponent
   ],
   imports: [
     BrowserModule,
