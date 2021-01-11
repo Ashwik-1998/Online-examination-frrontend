92c84d4b8a1f7b1cb99920dda1dc8b36f1b1e0cf
Vallari Chitnis <chitnisvallari@gmail.com>
2021-01-11 12:39:17 +0530

    commit done

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StudentService } from '../student.service';
+import { Login } from '../user-login/user-login.component';
 
 @Component({
   selector: 'app-admin-login',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent implements OnInit {
+ message : string;
+ login : Login = new Login();
+ response : any;
 
-  constructor() { }
+
+  constructor(private studentService  :  StudentService, private router :Router) { }
+
+  loginCheck()
+  {
+    alert(JSON.stringify(this.login));
+    this.studentService.login(this.login).subscribe(response =>{
+      alert(JSON.stringify(response));
+
+      
+
+    })
+  }
 
   ngOnInit() {
   }
 
+}
+
+
+export class AdminLogin{
+
+  email:string;
+  password: string;
 }
