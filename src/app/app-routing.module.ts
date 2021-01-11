2456e528ac4aa2934bfe05184e0d24f55f49a639
Vallari Chitnis <chitnisvallari@gmail.com>
2021-01-11 12:45:12 +0530

    committed user and admin login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+
+import { UserLoginComponent } from './user-login/user-login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'user-login',component:UserLoginComponent},
+{path:'admin-login',component:AdminLoginComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Login } from './user-login/user-login.component';
+import { AdminLogin } from './admin-login/admin-login.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login( login :Login) 
+  {
+    let url = "http://localhost:8081/userlogin";
+    return this.http.post(url,login);
+  }
+
+  adminlogin( login :AdminLogin) 
+  {
+    let url = "http://localhost:8081/adminlogin";
+    return this.http.post(url,login);
+  }
 }
